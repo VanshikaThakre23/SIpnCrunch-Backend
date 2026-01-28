c1914e8c194dff151b61c9d3fd14f018b6f0d8ed
Vanshika <vanshikathakre4@gmail.com>
2026-01-28 10:58:49 +0530

    removed admin logic from user.js

--- models/user.js
+++ models/user.js
@@ -1,29 +1,3 @@
-// import mongoose from "mongoose";
-// import bcrypt from "bcryptjs";
-
-// const userSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-//   email: { type: String, required: true, unique: true },
-//   password: { type: String, required: true },
-// });
-
-// // Hash password before saving
-// userSchema.pre("save", async function (next) {
-//   if (!this.isModified("password")) return next();
-//   const salt = await bcrypt.genSalt(10);
-//   this.password = await bcrypt.hash(this.password, salt);
-//   next();
-// });
-
-// // Compare password method
-// userSchema.methods.matchPassword = async function (enteredPassword) {
-//   return await bcrypt.compare(enteredPassword, this.password);
-// };
-
-// export default mongoose.model("User", userSchema);
-
-// ---------------added admin logic 
-
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
@@ -31,7 +5,6 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["user", "admin"], default: "user" }, // <-- added
 });
 
 // Hash password before saving
@@ -48,3 +21,30 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 };
 
 export default mongoose.model("User", userSchema);
+
+// // ---------------added admin logic 
+
+// import mongoose from "mongoose";
+// import bcrypt from "bcryptjs";
+
+// const userSchema = new mongoose.Schema({
+//   name: { type: String, required: true },
+//   email: { type: String, required: true, unique: true },
+//   password: { type: String, required: true },
+//   role: { type: String, enum: ["user", "admin"], default: "user" }, // <-- added
+// });
+
+// // Hash password before saving
+// userSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next();
+//   const salt = await bcrypt.genSalt(10);
+//   this.password = await bcrypt.hash(this.password, salt);
+//   next();
+// });
+
+// // Compare password method
+// userSchema.methods.matchPassword = async function (enteredPassword) {
+//   return await bcrypt.compare(enteredPassword, this.password);
+// };
+
+// export default mongoose.model("User", userSchema);
